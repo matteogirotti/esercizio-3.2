5d320a62262bfd8c56cf5de00f4b551028529d08
matteogirotti <matteogirotti10@gmail.com>
2024-09-25 14:42:14 +0200

    commit esercizio 3.2

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int num;
+    printf("inserisci un numero: ");
+    scanf("%d", &num);
+    if(num>0) {
+        printf("il numero %d e' positivo\n", num);
+    } else (num<0);{
+        printf("il numero %d e' negativo\n", num);
+    }
+}
